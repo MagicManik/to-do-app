f6cdeca387fb16facd9190f8bf58f8673eeb1529
MagicManik <manikislam1952@gmail.com>
2022-06-30 18:23:56 +0600

    firebase authentication done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import Header from './Components/Shared/Header/Header';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import Register from './Components/UserAccount/Register/Register';
+import Login from './Components/UserAccount/Login/Login';
 
 function App() {
   return (
     <div>
-
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/signin' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
+      </Routes>
+      <ToastContainer></ToastContainer>
     </div>
   );
 }
--- src/Components/Home/AddTask/AddTask.js
+++ src/Components/Home/AddTask/AddTask.js
@@ -1,60 +1,64 @@
-// import React from 'react';
+import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { toast } from 'react-toastify';
+import auth from '../../../firebase.init';
+import './AddTask.css';
 
-// const AddTask = () => {
+const AddTask = () => {
 
-//      const [user] = useAuthState(auth);
+    const [user] = useAuthState(auth);
 
-//     // handle form submit
-//     const handleTaskForm = event => {
+    // handle form submit
+    const handleTaskForm = event => {
 
-//         event.preventDefault();
-//         const email = user.email;
-//         const name = event.target.name.value;
-//         const description = event.target.description.value;
+        event.preventDefault();
+        const email = user.email;
+        const name = event.target.name.value;
+        const description = event.target.description.value;
 
-//         const task = { name, description, email };
+        const task = { name, description, email };
 
 
-//         fetch(' https://infinite-ridge-47501.herokuapp.com/tasks', {
-//             method: 'POST',
-//             headers: {
-//                 'content-type': 'application/json'
-//             },
-//             body: JSON.stringify(task)
-//         })
-//             .then(res => res.json())
-//             .then(data => {
+        fetch(' https://infinite-ridge-47501.herokuapp.com/tasks', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(task)
+        })
+            .then(res => res.json())
+            .then(data => {
 
-//                 // task added success হলে
-//                 if (data.insertedId) {
-//                     toast.success('Task is added successfully')
-//                 }
+                // task added success হলে
+                if (data.insertedId) {
+                    toast.success('Task is added successfully')
+                }
 
-//                 // task added success না হলে
-//                 else {
-//                     toast.error('Task Addition Failed')
-//                 }
-//             })
+                // task added success না হলে
+                else {
+                    toast.error('Task Addition Failed')
+                }
+            })
 
-//         event.target.reset();
-//     }
+        event.target.reset();
+    }
 
-//     return (
-//         <section className='task-form-container'>
-//         <div className='task-container'>
-//             <form onSubmit={handleTaskForm} className='task-form mx-auto'>
-//                 <h2 className='task-title'>Add Your Task</h2>
+    return (
+        <section className='task-form-container'>
+            <div className='task-container'>
+                <form onSubmit={handleTaskForm} className='task-form mx-auto'>
+                    <h2 className='task-title'>Add Your Task</h2>
 
-//                 <input className='task-input-field' type="text" name="name" id="" placeholder='Task name' required />
+                    <input className='task-input-field' type="text" name="name" id="" placeholder='Task name' required />
 
-//                 <textarea className='task-input-field' name="description" id="" cols="30" rows="3" placeholder='Task description' required></textarea>
+                    <textarea className='task-input-field' name="description" id="" cols="30" rows="3" placeholder='Task description' required></textarea>
 
-//                 <input className='add-task-button' type="submit" value="Add Task" />
+                    <input className='add-task-button' type="submit" value="Add Task" />
 
-//             </form>
-//         </div>
-//     </section>
-//     );
-// };
+                </form>
+            </div>
+        </section>
+    );
+};
 
-// export default AddTask;
+export default AddTask;
